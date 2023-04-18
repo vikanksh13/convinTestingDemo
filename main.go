8f8d56498753ec2bfad798423d8af318af17a32b
Vikanksh Pundhir <vikankshpundhir@Vikankshs-MacBook-Air.local>
2023-04-19 00:00:59 +0530

    main function added

--- main.go
+++ main.go
@@ -1,28 +1,40 @@
 package main
 
-// "convinTestingDemo/models"
-// "convinTestingDemo/service"
-// "convinTestingDemo/service_impl"
+import (
+	"convinTestingDemo/models"
+	serviceimpl "convinTestingDemo/service/service_impl"
+)
 
 func main() {
-	// testingCenter1 := models.TestingCenter{
-	// 	TestingCenterId: "1",
-	// 	XCoordinate:     2,
-	// 	YCoordinate:     5,
-	// 	NumberOfKits:    10,
-	// }
-	// testingCenter2 := models.TestingCenter{
-	// 	TestingCenterId: "1",
-	// 	XCoordinate:     2,
-	// 	YCoordinate:     5,
-	// 	NumberOfKits:    10,
-	// }
+	testingCenter1 := models.TestingCenter{
+		TestingCenterId: "1",
+		XCoordinate:     2,
+		YCoordinate:     5,
+		NumberOfKits:    10,
+	}
+	testingCenter2 := models.TestingCenter{
+		TestingCenterId: "2",
+		XCoordinate:     2,
+		YCoordinate:     5,
+		NumberOfKits:    10,
+	}
 
-	// testCenters := []models.TestingCenter{
-	// 	testingCenter1,
-	// 	testingCenter2,
-	// }
+	testCenters := []models.TestingCenter{
+		testingCenter1,
+		testingCenter2,
+	}
 
-	// service.IIngestTestingCenterService.IngestTestingCenter(testCenters)
+	user := models.User{
+		UserId:          "43",
+		UserXCoordinate: 2,
+		UserYCoordinate: 5,
+	}
 
+	testingCenterSvc := serviceimpl.TestingCenters{TestingCenters: testCenters}
+	// bookTestSvc := serviceimpl.NearestTestingCenter{}
+
+	testingCenterSvc.TestingCenterMap = make(map[string]models.TestingCenter)
+	testingCenterSvc.IngestTestingCenter(testCenters)
+
+	testingCenterSvc.BookTest(&user, 2)
 }
